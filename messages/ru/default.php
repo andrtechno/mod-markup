471c6bd0152b343b5ee2b83df5949cb84c9180b5
Andrew <andrew.panix@gmail.com>
2020-11-14 16:40:09 +0200

    init

--- messages/ru/default.php
+++ messages/ru/default.php
@@ -3,7 +3,6 @@ return [
     'MODULE_NAME' => 'Наценки',
     'MODULE_DESC' => 'Управление наценками магазина',
     'CREATE_DISCOUNT' => 'Добавить наценку',
-    'DAYS' => '{n, plural, =0{# дней} =1{# день} one{# день} few{# отзыва} many{# отзывов} other{# other}}',
     'NO_FOUND_MARKUP' => 'Наценка не найдена',
     'UPDATE' => 'Редактирование наценки',
     'CREATE' => 'Добавить наценку',
